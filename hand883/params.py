067873fec29eac59da54ace6b397a74a76516f06
Jan du Plessis <drjanduplessis@icloud.com>
2024-01-29 18:42:57 +0000

    Automated commit via Python script - 2024-01-29 18:42

--- hand883/params.py
+++ hand883/params.py
@@ -6,9 +6,9 @@ LOCAL_GIT_REPO = os.path.join(
     os.path.expanduser("~"),
     "code",
     "janduplessis883",
-    "project-hand883",
+    "hand883",
 )
 
 RAW_DATA = os.path.join(
-    os.path.expanduser("~"), "code", "janduplessis883", "project-hand883", "data"
+    os.path.expanduser("~"), "code", "janduplessis883", "hand883", "data"
 )
